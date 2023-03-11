7cf089e6ba5030f4060fecc141918b0aafbd0326
osasisorae <osasisorae@gmail.com>
2023-03-11 19:32:19 +0100

    modified react app to show mock data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
 const projects = [
   {"id":1,"title":"Test One","description":"Description One","start_date":"2022-10-01","end_date":"null","is_ongoing":true,"created_at":"2023-03-07T14:23:38.508537Z"},
   {"id":2,"title":"Test Two","description":"Project Test Two","start_date":"2020-10-01","end_date":"2022-06-01","is_ongoing":false,"created_at":"2023-03-07T14:24:19.138213Z"}
 ]
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { projects };
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.projects.map(item => (
+          <div key={item.id}>
+            <h1>{ item.title }</h1>
+            <p>{ item.description }</p>
+            <ul>
+              <li>{ item.is_ongoing }</li>
+              <li>{ item.start_date }</li>
+              <li>{ item.end_date }</li>
+            </ul>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
